8577176a1135bb1360e2073a2713cb9a06cd50fe
80010-09-08 <80010-09-08@exchange.ad.afpanet>
2020-07-08 16:41:53 +0200

    Ajout des fonctionnalité en php avec les testdb

---
+++ entete.php
@@ -0,0 +1,37 @@
+
+<body>
+  <!-- L'en tête du site -->
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-4"> <img class="logo" src="jarditou_html_zip/images/jarditou_logo.jpg" alt="logo jarditou">
+        </div>
+        <div class="titre col-8 text-center d-flex align-items-center justify-content-center">Tout le jardin</div>
+
+      </div>
+    </div>
+    </div>
+    <div>
+      <img class="banniere" src="jarditou_html_zip/images/promotion.jpg" alt="logo promotion">
+    </div>
+  </header>
+  <nav id="navbar" class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="main.html">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tableau.html">Tableau</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="formulaire.html">Formulaire</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="Assets/style.css">
+  <script src="app.js"></script>
+  <title>Jarditou</title>
+</head>
+<!-- Insertion de l'en-tête -->
+<?php
+include("entete.php");
+?>
+
+<!-- Le corps -->
+<div class="container-fluid">
+    <div class="row">
+      <div id="texte" class="col-md-8 col-6">
+        <article>
+          <h1>Acceuil</h1>
+          <hr>
+          <h2>L'entreprise</h2>
+          <p>Notre entreprise familiale met tout son savoir-faire à votre disposition dans le domaine du jardin et du
+            paysagisme.</p>
+          <p>Créée il y a 70 ans, notre entreprise vend fleurs, arbustes, matériel à main et motorisé.</p>
+          <p>Implantés à Amiens, nous intervenons dans tout le département de la Somme : Albert, Doullens, Péronne,
+            Abbeville, Corbie.</p>
+          <h2>Qualité</h2>
+          <p>Nous mettons à votre disposition un service personnalisé, avec 1 seul interlocuteur durant tout votre
+            projet.
+          </p>
+          <p>Vous serez éduit par notre expertise, nos compétences et notre sérieux.</p>
+          <h2>Devis gratuit</h2>
+          <p>Vous pouvez bien sûr contacter pour de plus amples informations ou pour une demande d'intervention. Vous
+            souhaitez un devis? Nous vous le réalisons gratuitement.</p>
+        </article>
+      </div>
+      <div id="colonne" class="col-md-4 col-6"> [COLONNE DROITE] </div>
+    </div>
+  </div>
+  <!-- Fin de page -->
+
+  <!-- Insertion du pied de page -->
+  <?php
+include("pieddepage.php");
+?>
+
+</html>
